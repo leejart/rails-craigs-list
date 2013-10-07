edd5308a10321be88cb52cff98864e7d6771fdb2
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-07 15:55:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CraigsList::Application.config.secret_key_base = '4f5f06b5ecdcac6ae824b230b0e69b8ff4fc44fd3f55967bf07d74caf293cb629c1759a2e332d2d16e20a8308f952b9bbde59d859d685745bb157dc7f8ecaf69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CraigsList::Application.config.session_store :cookie_store, key: '_craigs_list_session'
